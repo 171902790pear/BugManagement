2877ff8430b1d80c596e3339f4b9031bb09030b8
171902790pear <pear171902790@126.com>
2015-05-06 18:39:08 +0800

    sdfsafas

--- BugManagement.Repository/BaseRepository.cs
+++ BugManagement.Repository/BaseRepository.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using BugManagement.IRepository;
 using BugManagement.DomainModel;
+using BugManagement.Persistence;
+using Castle.Windsor;
 
 namespace BugManagement.Repository
 {
     public abstract class BaseRepository<TAggregateRoot> : IRepository<TAggregateRoot> where TAggregateRoot : IAggregateRoot
     {
+        private readonly IWindsorContainer _windsorContainer;
+        private DbContext _dbContext;
+        protected BaseRepository(IWindsorContainer windsorContainer)
+        {
+            _windsorContainer = windsorContainer;
+            _dbContext = _windsorContainer.Resolve<BugManagementDbContext>();
+        }
+
         public void Save(TAggregateRoot tRoot)
         {
             throw new System.NotImplementedException();
---
+++ BugManagement.Repository/WindsorInstaller/RepositoryInstaller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace BugManagement.Repository.WindsorInstaller
+{
+    public class RepositoryInstaller:IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IWindsorContainer>().Instance(container).LifestylePerWebRequest());
+        }
+    }
+}
